adc9b2d6504cb6a90b4b8a0b1e3e2409f797ee88
M4laise <stanescumatei02@gmail.com>
2025-12-23 20:17:51 +0200

    Am creat functia care verifica daca un fisier este symlink si daca e broken, in fisierul slcheck.sh

--- scripts/slcheck.sh
+++ scripts/slcheck.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-vf_broken_link() {
+vf_broken_link() 
+{
+	fileToCheck="$1"
+	filename=$(basename $fileToCheck)
+	
+	if [[ -L $fileToCheck ]]; then
+		if [[ -e $fileToCheck ]]; then
+			echo "$filename nu este broken!"
+		else
+			echo "$filename este broken!"
+		fi
+		
+	else echo "$filename nu este link!"
+		
+	fi
 }
 
 parcurgere() {
