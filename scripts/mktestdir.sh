99397162b4313f78bcce1c8013f7e4e594ba1733
M4laise <stanescumatei02@gmail.com>
2026-01-04 17:44:59 +0200

    Am imbunatatit scriptul care face testele, acum este vizibila adancimea pe fiecare fisier.

--- scripts/mktestdir.sh
+++ scripts/mktestdir.sh
@@ -19,23 +19,23 @@ touch $tempFilePath
 
 targetDir=$dirPath
 for (( i=1; i<=depth; i++ )); do # se creeaza depth foldere cu:
-	ln -s $tempFilePath "$targetDir/broken_link" #un broken link
-	ln -s "$HOME" "$targetDir/home_link" # un link functional catre folderul home al utilizatorului
+	ln -s $tempFilePath "$targetDir/broken_link$i" #un broken link
+	#ln -s "$HOME" "$targetDir/home_link" # un link functional catre folderul home al utilizatorului
 	
-	echo "fisier normal" > "$targetDir/normal file with spaces in name.txt" # un fisier cu spatii in nume
-	ln -s "$targetDir/normal file with spaces in name.txt" "$targetDir/link with spaces in name" # un link catre acesta
+	echo "fisier normal" > "$targetDir/normal file with spaces in name$i.txt" # un fisier cu spatii in nume
+	ln -s "$targetDir/normal file with spaces in name$i.txt" "$targetDir/link with spaces in name$i" # un link catre acesta
 	
-	ln -s "$targetDir/link_B" "$targetDir/link_A" # doua linkuri care arata unul la celalalt
-	ln -s "$targetDir/link_A" "$targetDir/link_B"
+	ln -s "$targetDir/link_B$i" "$targetDir/link_A$i" # doua linkuri care arata unul la celalalt
+	ln -s "$targetDir/link_A$i" "$targetDir/link_B$i"
 	
-	echo "fisier normal" > "$targetDir/normal_file.txt" # un fisier text
-	ln -s "$targetDir/normal_file.txt" "$targetDir/link_to_normal_file" # un link catre acesta
-	ln -s "$targetDir/link_to_normal_file" "$targetDir/link_to_link_to_normal_file" #un link catre acesta
+	echo "fisier normal" > "$targetDir/normal_file$i.txt" # un fisier text
+	ln -s "$targetDir/normal_file$i.txt" "$targetDir/link_to_normal_file$i" # un link catre acesta
+	ln -s "$targetDir/link_to_normal_file$i" "$targetDir/link_to_link_to_normal_file$i" #un link catre acesta
 	
-	echo "fisier normal" > "$targetDir/normal_file2.txt" # un fisier text
-	ln -s "$targetDir/normal_file2.txt" "$targetDir/link_to_normal_file2" # un link catre acesta
-	ln -s "$targetDir/link_to_normal_file2" "$targetDir/link_to_link_to_normal_file2" #un link catre acesta
-	rm "$targetDir/link_to_normal_file2" # stergem linkul din mijloc. acum lantul e rupt
+	echo "alt fisier normal" > "$targetDir/another_file$i.txt" # un fisier text
+	ln -s "$targetDir/another_file$i.txt" "$targetDir/broken_link_to_another_file$i" # un link catre acesta
+	ln -s "$targetDir/broken_link_to_another_file$i" "$targetDir/broken_link_to_link_to_another_file$i" #un link catre acesta
+	rm "$targetDir/broken_link_to_another_file$i" # stergem linkul din mijloc. acum lantul e rupt
 	
 	if (( i > 1 )); then
 		ln -s "$targetDir/.." "$targetDir/link_to_previous_directory" # daca depth > 1, se creeaza in fiecare folder un link catre folderul precedent
